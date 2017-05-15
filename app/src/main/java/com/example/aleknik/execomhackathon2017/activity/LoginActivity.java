3c34580d984768fda2f08cc18c8e69a9363bf423
Aleksandar Nikolic <nikolic95@gmail.com>
2017-05-15 21:07:52 +0200

    Login activity

---
+++ app/src/main/java/com/example/aleknik/execomhackathon2017/activity/LoginActivity.java
@@ -0,0 +1,13 @@
+package com.example.aleknik.execomhackathon2017.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.aleknik.execomhackathon2017.R;
+
+import org.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.activity_login)
+public class LoginActivity extends AppCompatActivity {
+
+}
--- app/src/main/java/com/example/aleknik/execomhackathon2017/activity/MainActivity.java
+++ app/src/main/java/com/example/aleknik/execomhackathon2017/activity/MainActivity.java
@@ -1,10 +1,12 @@
 package com.example.aleknik.execomhackathon2017.activity;
 
+import android.content.Intent;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.example.aleknik.execomhackathon2017.R;
@@ -14,6 +16,7 @@ import com.example.aleknik.execomhackathon2017.model.SaleItem;
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OnActivityResult;
 import org.androidannotations.annotations.OptionsItem;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.OptionsMenuItem;
@@ -23,7 +26,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
-@OptionsMenu(R.menu.layout_menu)
+@OptionsMenu(R.menu.main_menu)
 @EActivity(R.layout.activity_main)
 public class MainActivity extends AppCompatActivity {
     @ViewById
@@ -32,12 +35,29 @@ public class MainActivity extends AppCompatActivity {
     @Bean
     SaleItemAdapter saleItemAdapter;
 
+    private static final int LOGIN_REQUEST_CODE = 1;
+
     List<SaleItem> saleItems = new ArrayList<>();
 
     private boolean gridLayout;
 
-    @OptionsMenuItem
-    MenuItem layoutOption;
+    private boolean logedIn = false;
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuItem logout = menu.findItem(R.id.logout);
+        MenuItem login = menu.findItem(R.id.login);
+        if (logedIn) {
+            logout.setVisible(true);
+            login.setVisible(false);
+        } else {
+            logout.setVisible(false);
+            login.setVisible(true);
+        }
+        return true;
+    }
+
 
     @AfterViews
     void init() {
@@ -60,8 +80,20 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @OptionsItem(R.id.login)
+    void login() {
+        LoginActivity_.intent(this).startForResult(LOGIN_REQUEST_CODE);
+    }
+
+    @OnActivityResult(LOGIN_REQUEST_CODE)
+    public void onLogin(Intent data, int resultCode) {
+        if (resultCode == RESULT_OK) {
+        }
+    }
+
+
     @OptionsItem(R.id.layoutOption)
-    void changeLayout() {
+    void changeLayout(MenuItem layoutOption) {
 
         if (gridLayout) {
             recyclerView.setLayoutManager(new LinearLayoutManager(this,
